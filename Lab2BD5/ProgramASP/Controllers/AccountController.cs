1f7a3251c36167f000b8c90aa8bce41a80427e06
MihailEshman <esman6936@mail.com>
2023-12-25 11:04:43 +0300

    попытка добавить регистрацию

--- Lab2BD5/ProgramASP/Controllers/AccountController.cs
+++ Lab2BD5/ProgramASP/Controllers/AccountController.cs
@@ -1,23 +1,57 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgramASP.ViewModels.AccountModels;
 
 namespace ProgramASP.Controllers
 {
     public class AccountController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
         {
-            return View();
-        }
-        public IActionResult Login()
-        {
-            return View();
+            _userManager = userManager;
         }
 
-        public IActionResult Register()
+        public async Task<IActionResult> UsersList()
         {
+            ViewBag.Users = await _userManager.Users.ToListAsync();
+
             return View();
+        }
+        public IActionResult Login() => View();
+
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var user = new IdentityUser { PhoneNumber = model.PhoneNumber };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model); // Возвращение на страницу регистрации с сообщениями об ошибках
+            }
+
+            // Тут менять буду. 
+            if (_userManager.Users.Count() == 1)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+            else
+            {
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+            return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешной регистрации
         }
     }
 }
---
+++ Lab2BD5/ProgramASP/ViewModels/AccountModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramASP.ViewModels.AccountModels
+{
+    public class RegisterViewModel
+    {
+        [Phone]
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool? Rights { get; set; }
+    }
+}
